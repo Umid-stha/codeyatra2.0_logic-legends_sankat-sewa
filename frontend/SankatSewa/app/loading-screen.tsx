5867c18c56b29600f9f062a0476a5fe1dea74039
Sohan Maharjan <sohanmaharjan444@gmail.com>
2026-02-22 17:32:44 +0545

    Created a file for loading screeen

---
+++ frontend/SankatSewa/app/loading-screen.tsx
@@ -0,0 +1,48 @@
+import { Text, View, StyleSheet } from 'react-native';
+import ImageViewer from '@/components/ImageViewer';
+import Button from '@/components/Button';
+
+const PlaceholderImage = require('@/assets/images/android-icon-background.png');
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label='Choose a photo' />
+        <Button label='Choose a photo' />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  footerContainer: {
+    flex: 1/3,
+    alignItems: 'center'
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
