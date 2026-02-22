3938ea36b6ed9e6784620b2614c84de8a4b27eaf
Umid <shresthaumidccc@gmail.com>
2026-02-22 16:06:33 +0545

    react native setup

---
+++ frontend/SankatSewa/components/Button.tsx
@@ -0,0 +1,39 @@
+import { Pressable, View, Text, StyleSheet} from "react-native";
+
+type Prop = {
+    label: string;
+}
+
+export default function Button({label}: Prop) {
+    return (
+        <View style={styles.buttonContainer}>
+            <Pressable style={styles.button} onPress={() => alert('You Pressed a button')}>
+                <Text style={styles.buttonLabel}>{label}</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        width: 320,
+        height: 68,
+        marginHorizontal: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 3,
+    },
+    button: {
+        borderRadius: 10,
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+
+    },
+    buttonLabel: {
+        color: '#fff',
+        fontSize: 16,
+    }
+})
